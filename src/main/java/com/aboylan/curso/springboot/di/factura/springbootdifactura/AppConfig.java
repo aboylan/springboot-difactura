f3383cb8767c72bccbb80d1274b4d736c820798c
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-13 19:43:46 -0600

    PropertySource inyectando valores con @Value

---
+++ src/main/java/com/aboylan/curso/springboot/di/factura/springbootdifactura/AppConfig.java
@@ -0,0 +1,10 @@
+package com.aboylan.curso.springboot.di.factura.springbootdifactura;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:data.properties")
+public class AppConfig {
+
+}
