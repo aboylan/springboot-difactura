7cb3d47ae6cee2494bbd96883cf6660f0786823d
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-16 19:48:41 -0600

    Ciclo de vida del componente PreDestroy

--- src/main/java/com/aboylan/curso/springboot/di/factura/springbootdifactura/models/Invoice.java
+++ src/main/java/com/aboylan/curso/springboot/di/factura/springbootdifactura/models/Invoice.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Component
 public class Invoice {
@@ -32,7 +33,13 @@ public class Invoice {
     public void init() {
         System.out.println("Creando el componente de la factura");
         client.setName(client.getName().concat(" Pepe"));
-        description = description.concat(" del cliente: ").concat(client.getName()).concat(" ").concat(client.getLastname());
+        description = description.concat(" del cliente: ").concat(client.getName()).concat(" ")
+                .concat(client.getLastname());
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Destruyendo el componente o bean invoice!");
     }
 
     public Client getClient() {
